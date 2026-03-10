a21c879df8c21397aacb74ee875f79f4a6eac589
imzouwei-lang <imzouwei-lang@users.noreply.github.com>
2026-03-10 16:16:23 +0000

    Add test S3 bucket resource

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = try(aws_vpc.main[0].id, null)
-}
-
 output "account_id" {
   description = "AWS Account ID"
   value       = data.aws_caller_identity.current.account_id
+}
+
+output "test_bucket_name" {
+  description = "Test S3 Bucket Name"
+  value       = aws_s3_bucket.test.id
+}
+
+output "test_bucket_arn" {
+  description = "Test S3 Bucket ARN"
+  value       = aws_s3_bucket.test.arn
 }
