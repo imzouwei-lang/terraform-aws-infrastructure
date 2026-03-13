1520d189b0b4d07775cca0afb396c25204088f20
imzouwei-lang <imzouwei-lang@users.noreply.github.com>
2026-03-13 07:50:30 +0000

    Add EC2 instance: t2.medium in us-east-1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,13 @@ output "test_bucket_arn" {
   description = "Test S3 Bucket ARN"
   value       = aws_s3_bucket.test.arn
 }
+
+output "ec2_instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.web_server.id
+}
+
+output "ec2_instance_public_ip" {
+  description = "EC2 Instance Public IP"
+  value       = aws_instance.web_server.public_ip
+}
